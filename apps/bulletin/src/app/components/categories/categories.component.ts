6e036290901cd8a51c3a53f5527bec193e906256
alexander kozlov <akozlov@autospot.ru>
2023-10-27 13:52:09 +0300

    bulletin full

---
+++ apps/bulletin/src/app/components/categories/categories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {PopularProductCardComponent} from "../card/popular-product-card.component";
+
+@Component({
+  selector: 'bulletin-categories',
+  standalone: true,
+  imports: [CommonModule, PopularProductCardComponent],
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.scss'],
+})
+export class CategoriesComponent {}
--- apps/bulletin/src/app/views/home/home.component.ts
+++ apps/bulletin/src/app/views/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PopularComponent} from "../../components/popular/popular.component";
+import {CategoriesComponent} from "../../components/categories/categories.component";
 
 @Component({
   selector: 'bulletin-home',
   standalone: true,
-  imports: [CommonModule, PopularComponent],
+  imports: [CommonModule, CategoriesComponent, PopularComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
